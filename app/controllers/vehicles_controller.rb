a27fc9da76bd3cfcbdd58f17c33b49b0a177f32b
Thomas Belpomo <thomas.belpomo@hotmail.fr>
2023-11-21 12:31:53 +0100

    vehicle_new en cours

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,2 +1,17 @@
 class VehiclesController < ApplicationController
+  def new
+    @vehicle = Vehicle.new
+  end
+
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+    @vehicle.save
+    redirect_to new_vehicle_path(@vehicle)
+  end
+
+  private
+
+  def vehicle_params
+    params.require(:vehicle).permit(:name, :description, :price)
+  end
 end
