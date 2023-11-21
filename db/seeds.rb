91085ab90727fd6bbc4c1c2f010500796ad6fa5e
carine randrianarijaona carinerandrianarijaona@gmail.com <carinerandrianarijaona@gmail.com>
2023-11-21 13:59:35 +0100

    wip

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@ user_2 = User.create!(first_name: "Thomas", last_name: "Wagon", postal_code: 750
 user_3 = User.create!(first_name: "Corentin", last_name: "Wagon", postal_code: 75000, adress: "rue de la paix", city: "Paris",  email: "toto+4@toto.fr", password: "azerty")
 user_4 = User.create!(first_name: "Carine", last_name: "Wagon", postal_code: 75000, adress: "rue de la paix", city: "Paris",  email: "toto+5@toto.fr", password: "azerty")
 
-Vehicle.create!(name: "Ferrari", description: "blabla", category: "bloblo", price: 7, user_id: user_1.id)
+vehicle_1 = Vehicle.create!(name: "Voiture volante", description: "Un véhicule qui peut se transformer en avion, offrant une solution de transport polyvalente pour éviter les embouteillages.", category: "voiture", price: 7, user_id: user_1.id)
+vehicle_2 = Vehicle.create!(name: "Train à grande vitesse maglev", description: "Un train utilisant la lévitation magnétique pour atteindre des vitesses élevées, offrant un moyen rapide et confortable de voyager entre les villes.", category: "train", price: 80, user_id: user_2.id)
