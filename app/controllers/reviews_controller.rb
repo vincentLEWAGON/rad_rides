e87097d6e4ed86af9da7f277386d2815e80cb754
carine randrianarijaona carinerandrianarijaona@gmail.com <carinerandrianarijaona@gmail.com>
2023-11-22 10:00:50 +0100

    wip

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,17 @@ class ReviewsController < ApplicationController
   def my_reviews
     @my_reviews = Review.all
   end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+    if @review.save
+      flash.notice = "Commentaire envoyé"
+      redirect_to @reviews
+    end
+  end
 end
