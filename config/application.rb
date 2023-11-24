df276902699f63f30ed80b5678f49734e4d4eb8c
Vincent Schmitt <vincentschmitt@free.fr>
2023-11-24 10:05:52 +0100

    reset application.rb and development.rb

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,13 @@ module RadRides
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    # config.assets.enable = false
-    # config.assets.precompile
+    config.assets.enable = false
+    config.assets.precompile
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    # config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
     #
